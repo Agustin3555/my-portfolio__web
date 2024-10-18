c6bd1b175e9ab408ff3c0f0c77c02c6b5af6902c
Agustin3555 <Agustin3555@hotmail.com>
2024-10-18 00:58:56 -0300

    feat: Cambios 4.

--- src/components/Home/components/Skills/Skills.styled.ts
+++
@@ -1,9 +0,0 @@
-import { NOT_FONT_SIZE } from "@/styles";
-import styled from "@emotion/styled";
-
-export const Component = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: flex-start;
-  gap: ${NOT_FONT_SIZE.m};
-`;
