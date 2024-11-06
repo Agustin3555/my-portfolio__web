0228dbd0fe7e8e7479e4a2076ccce16c349ec3df
Agustin3555 <Agustin3555@hotmail.com>
2024-11-06 17:08:28 -0300

    feat(Section, 404, resume, Tabbed)

--- src/layouts/BasicHeader/BasicHeader.styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-import { VARS } from "@/styles";
-
-export const Component = styled.header`
-  display: flex;
-  flex-direction: column;
-  gap: calc(${VARS.size.gold} * 4);
-`;
